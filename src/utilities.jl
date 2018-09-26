bc3b11bca7eecdd45de60b09024cca8c209da2cc
Martin <mklemann@mailbox.org>
2018-09-26 20:48:10 +0200

    fix array2string
    fix array2string

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,8 +1,8 @@
-function array2string(x)::Array{String,1}
+function array2string(x::Array{String, 1})
     prod(x .* ", ")[1:end-2]
 end
 
-function array2string(x)::Array{Number, 1}
-	map(x -> string(x), x)
+function array2string(x::Array{<:Number, 1})
+	x = map(y -> string(y), x)
 	array2string(x)
 end
