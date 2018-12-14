202b1ad01f1a1f2c96eaa6d7ad0988027a685a9a
Martin <mklemann@mailbox.org>
2018-12-14 13:36:39 +0100

    add more and more

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,10 @@ using SQLite, NamedArrays
 import Statistics.mean, DataFrames.describe
 const MOI = JuMP.MathOptInterface
 
+include("types.jl")
+include("utilities.jl")
+# include("database.jl")
+include("decay.jl")
+include("model.jl")
+
 include("./test_utilities.jl")
