57e5dcf53a7cf98db56ebe374c45c79bc5d09604
Martin <mklemann@mailbox.org>
2018-12-14 13:51:57 +0100

    correcting paths

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,10 +5,10 @@ using SQLite, NamedArrays
 import Statistics.mean, DataFrames.describe
 const MOI = JuMP.MathOptInterface
 
-include("types.jl")
-include("utilities.jl")
-# include("database.jl")
-include("decay.jl")
-include("model.jl")
+include("../src/types.jl")
+include("../src/utilities.jl")
+# include("../src/database.jl")
+include("../src/decay.jl")
+include("../src/model.jl")
 
 include("./test_utilities.jl")
