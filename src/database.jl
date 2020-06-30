128a3a979ac21c6776c76fe4a8f6d961564c43ed
Martin <mklemann@mailbox.org>
2020-06-30 14:39:44 +0200

    latest fixes

--- src/database.jl
+++ src/database.jl
@@ -1,28 +1,29 @@
 "Pfad zur Datenbank"
 #const db = joinpath(dirname(dirname(Base.functionloc(NuVe.eval, Tuple{Nothing})[1])), "src", "nvdb-v3.sqlite")
-const db = joinpath("src", "nvdb-v3.sqlite")
+# const db = joinpath("src", "nvdb-v3.sqlite")
 xf_param = XLSX.readxlsx(joinpath("src", "Parameter.xlsx"))
-"""
-    readDb(s::String)
-Gibt die angegebene Tabelle vollständig als DataFrame zurück.
-"""
-function readDb(s::String)
-	DBInterface.execute(nvdb(), "select * from " * s) |> DataFrame
-end
+xf_ana = XLSX.readxlsx(joinpath("src", "Vollanalysen.xlsx"))
+# """
+#     readDb(s::String)
+# Gibt die angegebene Tabelle vollständig als DataFrame zurück.
+# """
+# function readDb(s::String)
+# 	DBInterface.execute(nvdb(), "select * from " * s) |> DataFrame
+# end
 
-"""
-    readDb(s::String, arg::Array{String,1})
+# """
+#     readDb(s::String, arg::Array{String,1})
 
-Gibt die angegebene Tabelle mit den angegebenen Spalten als DataFrame zurück.
-"""
-function readDb(tab::String, arg::Array{String,1})
-	DBInterface.execute(nvdb(), "select " * array2string(arg) * " from " * tab) |> DataFrame
-end
+# Gibt die angegebene Tabelle mit den angegebenen Spalten als DataFrame zurück.
+# """
+# function readDb(tab::String, arg::Array{String,1})
+# 	DBInterface.execute(nvdb(), "select " * array2string(arg) * " from " * tab) |> DataFrame
+# end
 
-"Datenbank mit allen Nuklidvektoren, Proben, Halbwertszeiten und Freigabewerten"
-function nvdb()
-	SQLite.DB(db)
-end
+# "Datenbank mit allen Nuklidvektoren, Proben, Halbwertszeiten und Freigabewerten"
+# function nvdb()
+# 	SQLite.DB(db)
+# end
 
 "Die Freigabewerte aller Nuklide"
 # const clearance_val = df2namedarray(readDb("clearance_val"), "path", "nuclide")
@@ -36,10 +37,22 @@ const clearance_val = fun_clearance_val()
 const nu_names = clearance_val.dicts[2] |> keys .|> String
 
 "Die Halbwertszeiten aller Nuklide"
-const hl = DBInterface.execute(nvdb(), "select " * array2string(nu_names) * " from halflife") |> DataFrame
+function fun_hl()
+	sheet_hl = xf_param["Halbwertszeiten"]
+	hltmp = convert(Matrix{Float64}, [sheet_hl["B2:B33"][i] .* (sheet_hl["C2:C33"][i] == "a" ? 365.25 : 1) for i=1:length(nu_names)]')
+	DataFrame(hltmp, Symbol.(sheet_hl["A2:A33"] |> vec))
+end
+# const hl = DBInterface.execute(nvdb(), "select " * array2string(nu_names) * " from halflife") |> DataFrame
+const hl = fun_hl()
 
 "Die Messeffizienzen für die verschiedenen Messverfahren und Nuklide"
-const ɛ = df2namedarray(readDb("efficiency"), "method", "nuclide")
+function fun_ɛ()
+	sheet_ɛ = xf_param["Messeffizienzen"]
+	q = NamedArray(convert(Matrix{Float64}, coalesce.(sheet_ɛ["B2:F33"], 0)'), convert(Tuple{Vector{String}, Vector{String}}, (sheet_ɛ["B1:F1"] |>vec , sheet_ɛ["A2:A33"] |> vec)))
+	NamedArray(q ./ q[:, "Co60"], convert(Tuple{Vector{String}, Vector{String}}, (sheet_ɛ["B1:F1"] |>vec , sheet_ɛ["A2:A33"] |> vec)))
+end 
+# const ɛ = df2namedarray(readDb("efficiency"), "method", "nuclide")
+const ɛ = fun_ɛ()
 const ɛᵀ = ɛ'
 
 "Das Inverse der Freigabewerte"
@@ -47,13 +60,13 @@ const f = NamedArray( 1 ./ clearance_val.array, clearance_val.dicts, clearance_v
 const fᵀ = f'
 
 """
-    getNuclideTypes(s::String)
+#     getNuclideTypes(s::String)
 
-Gibt alle Nuklide eines bestimmten Strahlungstypes (α, β oder γ) zurück.
-"""
-function getNuclideTypes(t::String)
-	 DBInterface.execute(nvdb(),  "select nuclide from nuclide_decayType where decayType = '" * t * "'" ) |> DataFrame
-end
+# Gibt alle Nuklide eines bestimmten Strahlungstypes (α, β oder γ) zurück.
+# """
+# function getNuclideTypes(t::String)
+# 	 DBInterface.execute(nvdb(),  "select nuclide from nuclide_decayType where decayType = '" * t * "'" ) |> DataFrame
+# end
 
 """
 	getSampleFromSource(x::Symbol)
